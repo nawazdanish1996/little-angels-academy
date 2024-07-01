d5ee05d5e663251890d7ab684f61a2abb30004d2
Nawaz Danish <contact.to.nd@gmail.comgit config --global user.email contact.to.nd@gmail.comgit config --global user.email contact.to.nd@gmail.com>
2024-07-02 01:00:43 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Suspense, lazy } from "react";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
@@ -9,10 +9,11 @@ const Home = lazy(()=> import("./pages/Home"));
 const About = lazy(()=> import("./pages/About"))
 const Contact = lazy(()=> import("./pages/Contact"))
 const Teachers = lazy(()=> import("./pages/Teachers"))
+const NotFound = lazy(()=> import("./pages/NotFound"));
 
 const App = () => {
   return (
-    <HashRouter>
+    <Router>
       <Marquees />
       <GoToTop />
       <Navbar />
@@ -22,9 +23,10 @@ const App = () => {
         <Route path="/about" element={<Suspense fallback="loading..."><About /></Suspense>} />
         <Route path="/contact" element={<Suspense fallback="loading..."><Contact /></Suspense>} />
         <Route path="/our_teachers" element={<Suspense fallback="loading..."><Teachers /></Suspense>} />
+        <Route path="*" element={<Suspense fallback="loading..."><NotFound /></Suspense>} />
       </Routes>
       <Footer />
-    </HashRouter>
+    </Router>
   )
 }
 
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,14 @@
+import "../css/NotFound.css";
+
+const NotFound = () => {
+  return (
+    <div className="notfound">
+        <div>
+            <h1>404 || Page not found</h1>
+            <a href="/">Go to homepage</a>
+        </div>
+    </div>
+  )
+}
+
+export default NotFound
