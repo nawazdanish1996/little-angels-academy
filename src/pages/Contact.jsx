79e377780c7d816891e42ef603c5da5caab15f57
Nawaz Danish <contact.to.nd@gmail.comgit config --global user.email contact.to.nd@gmail.comgit config --global user.email contact.to.nd@gmail.com>
2024-07-06 22:20:01 +0530

    updated

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,8 +3,17 @@ import "../css/Contact.css"
 const Contact = () => {
   return (
     <div className="contact">
-      <h1>Contact Us</h1>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque et voluptatem natus vel magnam consectetur ipsa, perferendis excepturi, molestias dicta quas odit repellendus. Quasi aspernatur reiciendis, eos, omnis reprehenderit esse aliquid, laboriosam ducimus et earum obcaecati hic! Hic maiores sequi unde totam voluptatibus fugiat quibusdam natus dolores magni facere molestiae obcaecati, quas beatae placeat. Ducimus ab deleniti aut deserunt eos corporis excepturi labore officia modi ipsam adipisci aperiam, minima cum? Temporibus nulla molestias explicabo aliquid asperiores ipsum ea totam labore voluptates alias. Fuga aut ea vel ipsum facilis pariatur velit maxime? Eaque, cupiditate sint. Amet ipsum facere repudiandae doloribus libero!</p>
+      <div className="contact-details">
+        <h1>Contact Us</h1>
+        <div className="contact-address">
+          <p>Address: Lodhan Chowk, Goalpokher, Uttar Dinajpur, W/B, 733210</p>
+          <p>Mob. No.: +91 - 8391809802</p>
+          <p>Email: contact.to.nd@gmail.com</p>
+        </div>
+      </div>
+      <div className="contact-map">
+        <iframe src="" frameBorder="0"></iframe>
+      </div>
     </div>
   )
 }
